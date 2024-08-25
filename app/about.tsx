7283f83efea3ed696dca8f59c434fe015b0e7fc9
Eirik <eirik.imrik@gmail.com>
2024-08-25 15:29:25 +0200

    added an about page and fixed the bacground issue

---
+++ app/about.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const About = () => {
+  return (
+    <View>
+      <Text>
+        {" "}
+        Welcome to Ålesund Weather, your dedicated weather forecasting app for
+        Ålesund, Norway. Our mission is to provide residents and visitors with
+        the simple and accurate weather information for the city of Ålesund.{" "}
+        This website is built and maintained by Eirik Imrik. For any inquiries,
+        please contact me at eirik.imrik@gmail.com. Thank you for visiting
+        Ålesund Weather!
+      </Text>
+    </View>
+  );
+};
+
+export default About;
+
+const styles = StyleSheet.create({});
--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,57 @@
 import { Text, View, StyleSheet, Dimensions } from "react-native";
-import { LinearGradient } from 'expo-linear-gradient';
-
+import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Link } from "expo-router";
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    fullScreen: {
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height,
+    },
+  });
 
-  const styles = StyleSheet.create({ fullScreen: { width: Dimensions.get('window').width, height: Dimensions.get('window').height, }, });
+  const gradientStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width: "100%",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
+
+  const viewStyles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        
-      }}
-    >
-      <LinearGradient
-      colors={ [ '#04b3f8', 'rgb(182, 221, 236)']}
-
-      start={[0, 0]}
-      end={[1, 1]}
-      style={{
-        flex: 1,
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      
-      >
-        <Text>Edit app/index.tsx to edit this screen.</Text>
-
-      </LinearGradient>
-      
-      
-    </View>
+    <SafeAreaProvider>
+      <View style={viewStyles.container}>
+        <LinearGradient
+          colors={["#04b3f8", "rgb(182, 221, 236)"]}
+          start={[0, 0]}
+          end={[1, 1]}
+          style={gradientStyles.container}
+        >
+          <Text>Edit app/index.tsx to edit this screen.</Text>
+          <Link
+            href="/about"
+            style={{
+              color: "white",
+              backgroundColor: "black",
+              padding: 10,
+              borderRadius: 5,
+              marginTop: 10,
+            }}
+          >
+            About
+          </Link>
+        </LinearGradient>
+      </View>
+    </SafeAreaProvider>
   );
 }
